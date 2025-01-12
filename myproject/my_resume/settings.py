a5a4cf4cf94bd5e821feb9352b79d17f41c9bb54
weldonhack <weldonkenei30@gmail.com>
2025-01-12 16:25:04 +0300

    migrated the database

--- myproject/my_resume/settings.py
+++ myproject/my_resume/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-v8bzoj5)*&_%x-yy7o*z-2$*m1uuo*hbtb(n)%@bboej@%wkox'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '*'
@@ -79,10 +79,21 @@ WSGI_APPLICATION = 'my_resume.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+DATABASES ={
+    'default':{
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME':'railway',
+        'USER':'postgres',
+        'PASSWORD':'TmzeUJzYZIFcOtVyZPIwLeyJmWxFanKv',
+        'HOST':'roundhouse.proxy.rlwy.net',
+        'PORT':'25847',
     }
 }
 
